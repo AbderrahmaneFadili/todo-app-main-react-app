eff5dcfcc2b10507596516cbd346bc17a04fe260
Abderrahmane Fadili <abdouelrfadili@gmail.com>
2021-04-17 11:13:24 +0000

    Setup Redux & Firestore & Firebase

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,10 +1,14 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import themeReducer from "./reducers/themeReducers";
+import { firebaseReducer } from "react-redux-firebase";
+import { firestoreReducer } from "redux-firestore";
 import thunk from "redux-thunk";
 import { getFirebase } from "react-redux-firebase";
 
 const rootReducer = combineReducers({
   themeReducer,
+  firebase: firebaseReducer,
+  firestore: firestoreReducer,
 });
 
 const store = createStore(
